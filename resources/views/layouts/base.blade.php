e8f107a0d2cb7b83096d68a3e0c5229012f7586f
Kyle Anderson <kylea@schwebdesign.com>
2021-03-13 15:19:29 -0500

    added alpinejs import

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,6 +22,7 @@
 
         <!-- Scripts -->
         <script src="{{ url(mix('js/app.js')) }}" defer></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
