e1bb480cd85acef8f697d96d860f6784d0d8601e
Kyle Anderson <kylea@schwebdesign.com>
2021-03-13 15:19:19 -0500

    added scopes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+
+    /**
+     * @param mixed $query
+     * @return Builder
+     */
+    public function scopeCompleted($query): Builder
+    {
+        return $query->where('completed', true);
+    }
+
+    /**
+     * @param mixed $query
+     * @return Builder
+     */
+    public function scopeIncomplete($query): Builder
+    {
+        return $query->where('completed', false);
+    }
 }
